eb70304b7d91d543c4b9814e416d1e866627fdd8
Pawin Vongmasa <pawin@google.com>
2019-03-12 19:56:00 -0700

    msm8953-common: enable codec2
    Test: make cts -j123 && cts-tradefed run cts-dev -m \
    CtsMediaTestCases --compatibility:module-arg \
    CtsMediaTestCases:include-annotation:\
    android.platform.test.annotations.RequiresDevice
    
    Bug: 128456533
    Change-Id: Ibe051f4b19c2dbb0a208e751a3b7a332a329b447
    Signed-off-by: KenHV <kenhv@null.net>

--- prop.mk
+++ prop.mk
@@ -142,6 +142,7 @@ ro.gps.agps_provider=1
 PRODUCT_PROPERTY_OVERRIDES += \
 av.debug.disable.pers.cache=1 \
 debug.sf.enable_hwc_vds=1 \
+debug.media.codec2=2 \
 media.aac_51_output_enabled=true \
 media.msm8956hw=0 \
 media.stagefright.audio.sink=280 \
